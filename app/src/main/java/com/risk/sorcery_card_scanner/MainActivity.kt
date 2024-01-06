090b5cffb0beabeb5dd139744fc50d3c27e98853
Trevor Soare <soaringexteriors@gmail.com>
2024-01-05 22:37:11 -0800

    Initial commit

---
+++ app/src/main/java/com/risk/sorcery_card_scanner/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.risk.sorcery_card_scanner
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.risk.sorcery_card_scanner.ui.theme.Sorcery_card_scannerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Sorcery_card_scannerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Sorcery_card_scannerTheme {
+        Greeting("Android")
+    }
+}
